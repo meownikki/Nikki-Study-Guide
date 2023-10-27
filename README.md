e7f198a895c69dc1113be72604b6b07e7a49c070
Carlo Santos <rcarlosantos89@gmail.com>
2023-10-27 10:33:44 -0500

    created a readme and sample exercise file

---
+++ README.md
@@ -0,0 +1,16 @@
+# README
+
+A readme is a good starting point to understand or tell other developers how to
+use/collaborate/run on your project.
+
+## Review
+- Basic JS
+  - Functions
+- Exporting Files
+- Testing
+- Git
+
+
+## Git
+### Common Commands
+- `git init` - initialize git repo
---
+++ firstExercise.js
@@ -0,0 +1,11 @@
+// Writing local JS Lesson
+// This is just a js file locally
+// JS was traditionally used for front-end development in the browser.
+// Node.js gives us the ability to run JS on the server.
+
+// In order to run your code with Node.js you need to run the following command in your terminal:
+// node <pathtoyour/filename.js>
+console.log("Hello World!");
+
+
+// Path: secondExercise.js
